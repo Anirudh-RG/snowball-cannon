076af14dd945b274decd1fddc9878722dc9ffc46
Anirudh-RG <i-Anirudh@roundglass.com>
2025-02-10 04:48:47 +0000

    updated jenkinsfile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>HELLO WORLD </h1>
+        <h2>THIS IS DEELOPMENT BABBY</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
